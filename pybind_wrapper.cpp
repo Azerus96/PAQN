89b1053d49f360c9c0856173cc26528ce2794657
Azerus96 <gencinski1996@gmail.com>
2025-07-05 08:10:53 +0300

    Update pybind_wrapper.cpp

--- pybind_wrapper.cpp
+++ pybind_wrapper.cpp
@@ -16,20 +16,20 @@ PYBIND11_MODULE(ofc_engine, m) {
         .def("pop_all", &InferenceQueue::pop_all)
         .def("wait", &InferenceQueue::wait, py::call_guard<py::gil_scoped_release>());
 
+    // ИЗМЕНЕНО: Обновляем биндинги для InferenceRequest
     py::class_<InferenceRequest>(m, "InferenceRequest")
         .def_readonly("infoset", &InferenceRequest::infoset)
-        .def_readonly("num_actions", &InferenceRequest::num_actions)
+        // Добавляем доступ к новому полю
+        .def_readonly("action_vectors", &InferenceRequest::action_vectors)
         .def("set_result", [](InferenceRequest &req, std::vector<float> result) {
             req.promise.set_value(result);
         });
 
-    // ИЗМЕНЕНО: Обновляем биндинги для SharedReplayBuffer
     py::class_<ofc::SharedReplayBuffer>(m, "SharedReplayBuffer")
         .def(py::init<uint64_t>(), py::arg("capacity"))
         .def("get_count", &ofc::SharedReplayBuffer::get_count)
         .def("get_head", &ofc::SharedReplayBuffer::get_head)
         .def("sample", [](ofc::SharedReplayBuffer &buffer, int batch_size) {
-            // Создаем три numpy массива
             auto infosets_np = py::array_t<float>(batch_size * ofc::INFOSET_SIZE);
             auto actions_np = py::array_t<float>(batch_size * ofc::ACTION_VECTOR_SIZE);
             auto regrets_np = py::array_t<float>(batch_size);
@@ -41,12 +41,10 @@ PYBIND11_MODULE(ofc_engine, m) {
                 static_cast<float*>(regrets_np.request().ptr)
             );
 
-            // Решейпим
             infosets_np.resize({batch_size, ofc::INFOSET_SIZE});
             actions_np.resize({batch_size, ofc::ACTION_VECTOR_SIZE});
-            regrets_np.resize({batch_size, 1}); // Для консистентности с MSELoss
+            regrets_np.resize({batch_size, 1});
 
-            // Возвращаем кортеж из трех массивов
             return std::make_tuple(infosets_np, actions_np, regrets_np);
         }, py::arg("batch_size"));
 
