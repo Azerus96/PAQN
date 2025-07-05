d26ef68c84e4e249c4a4ed0c3686feda7e711ff9
Azerus96 <gencinski1996@gmail.com>
2025-07-05 22:55:15 +0300

    Update DeepMCCFR.hpp

--- cpp_src/DeepMCCFR.hpp
+++ cpp_src/DeepMCCFR.hpp
@@ -17,10 +17,7 @@ public:
     void run_traversal();
 
 private:
-    // --- ИЗМЕНЕНИЕ: ДОБАВЛЕНА КОНСТАНТА EPSILON ---
-    // Вероятность выбора случайного действия для исследования (10%)
-    static constexpr double EPSILON = 0.25; 
-    // --- КОНЕЦ ИЗМЕНЕНИЯ ---
+    // --- УДАЛЕНО: Константа EPSILON для ε-greedy больше не нужна. ---
 
     HandEvaluator evaluator_;
     SharedReplayBuffer* replay_buffer_; 
@@ -28,7 +25,10 @@ private:
     size_t action_limit_;
     std::mt19937 rng_;
 
-    std::map<int, float> traverse(GameState& state, int traversing_player);
+    // --- ИЗМЕНЕНИЕ: Добавлен параметр is_root для отслеживания корневого узла обхода. ---
+    // Это необходимо, чтобы применять шум Дирихле только один раз в начале обхода для каждого игрока.
+    std::map<int, float> traverse(GameState& state, int traversing_player, bool is_root);
+    
     std::vector<float> featurize(const GameState& state, int player_view);
 };
 
