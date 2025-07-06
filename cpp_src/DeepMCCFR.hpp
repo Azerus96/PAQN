cd40410e1a9516bc0f079599f7a70fe08c37ddea
Azerus96 <gencinski1996@gmail.com>
2025-07-06 23:21:34 +0300

    Update DeepMCCFR.hpp

--- cpp_src/DeepMCCFR.hpp
+++ cpp_src/DeepMCCFR.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <pybind11/pybind11.h>
-#include <pybind11/functional.h> // Для std::function
+#include <pybind11/functional.h>
 #include "game_state.hpp"
 #include "hand_evaluator.hpp"
 #include "SharedReplayBuffer.hpp"
@@ -8,16 +8,17 @@
 #include <map>
 #include <memory>
 #include <random>
-#include <functional> // Явно добавим для std::function
+#include <functional>
+#include <atomic> // Для атомарного счетчика
 
 namespace py = pybind11;
 
 namespace ofc {
 
 // Тип для Python-функции, которую мы будем вызывать для Policy-инференса
-// Принимает: инфосет, векторы действий, функцию для ответа
 using PolicyInferenceCallback = std::function<
     void(
+        uint64_t, // traversal_id
         const std::vector<float>&, 
         const std::vector<std::vector<float>>&,
         const std::function<void(std::vector<float>)>&
@@ -25,9 +26,9 @@ using PolicyInferenceCallback = std::function<
 >;
 
 // Тип для Python-функции, которую мы будем вызывать для Value-инференса
-// Принимает: инфосет, функцию для ответа
 using ValueInferenceCallback = std::function<
     void(
+        uint64_t, // traversal_id
         const std::vector<float>&,
         const std::function<void(float)>&
     )
@@ -36,7 +37,6 @@ using ValueInferenceCallback = std::function<
 
 class DeepMCCFR {
 public:
-    // Конструктор теперь принимает callback-функции
     DeepMCCFR(size_t action_limit, SharedReplayBuffer* policy_buffer, SharedReplayBuffer* value_buffer, 
               PolicyInferenceCallback policy_cb, ValueInferenceCallback value_cb);
     
@@ -51,8 +51,12 @@ private:
     size_t action_limit_;
     std::mt19937 rng_;
     std::vector<float> dummy_action_vec_;
+    
+    // Статический счетчик для уникальных ID обходов
+    static std::atomic<uint64_t> traversal_counter_;
 
-    std::map<int, float> traverse(GameState& state, int traversing_player, bool is_root);
+    // Сигнатура traverse изменена для передачи ID
+    std::map<int, float> traverse(GameState& state, int traversing_player, bool is_root, uint64_t traversal_id);
     std::vector<float> featurize(const GameState& state, int player_view);
 };
 
