49311a072385ebae77197ff330b8a0c8cdcf5957
Azerus96 <gencinski1996@gmail.com>
2025-07-09 08:41:09 +0300

    Update InferenceQueue.hpp

--- cpp_src/InferenceQueue.hpp
+++ cpp_src/InferenceQueue.hpp
@@ -12,28 +12,20 @@ namespace py = pybind11;
 
 namespace ofc {
 
-// Структура запроса от C++ к Python
 struct InferenceRequest {
     uint64_t id;
     bool is_policy_request;
-    // --- ИЗМЕНЕНИЕ ---: Вместо вектора float для инфосета, передаем вектор int для сырого состояния
-    std::vector<int> raw_state; 
-    // Для policy-запроса, содержит векторы действий. Для value - пустой.
+    std::vector<float> infoset; 
     std::vector<std::vector<float>> action_vectors; 
 };
 
-// Структура ответа от Python к C++
 struct InferenceResult {
     uint64_t id;
     bool is_policy_result;
-    // Для policy-ответа, содержит логиты. Для value - один элемент (сама ценность).
     std::vector<float> predictions; 
 };
 
-// Очередь запросов (C++ -> Python)
 using InferenceRequestQueue = py::object;
-
-// Очередь результатов (Python -> C++)
 using InferenceResultQueue = py::object;
 
 } // namespace ofc
