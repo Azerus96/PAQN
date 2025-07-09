7a31b37b846b08822ff4baea85c68d8d09d94f51
Azerus96 <gencinski1996@gmail.com>
2025-07-09 08:38:10 +0300

    Update constants.hpp

--- cpp_src/constants.hpp
+++ cpp_src/constants.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
 namespace ofc {
-    // Единый источник правды для размера инфосета
-    constexpr int INFOSET_SIZE = 1486;
-    // НОВАЯ КОНСТАНТА: Размер вектора, описывающего действие
-    // 52 карты * 4 возможных слота (top, middle, bottom, discard)
+    constexpr int NUM_SUITS = 4;
+    constexpr int NUM_RANKS = 13;
+    
+    // Каналы признаков для представления состояния игры
+    constexpr int NUM_FEATURE_CHANNELS = 16; // 8 карт + 5 улиц + 3 скаляра
+    constexpr int INFOSET_SIZE = NUM_FEATURE_CHANNELS * NUM_SUITS * NUM_RANKS;
+
     constexpr int ACTION_VECTOR_SIZE = 208;
-    // ДОБАВЛЕНА КОНСТАНТА для исправления ошибки компиляции
-    constexpr int SUIT_COUNT = 4;
 }
