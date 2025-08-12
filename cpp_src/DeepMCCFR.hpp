736a83c937f6e488796ca6f454f332d1ae6dfadb
Azerus96 <gencinski1996@gmail.com>
2025-08-12 20:41:53 +0300

    Update DeepMCCFR.hpp

--- cpp_src/DeepMCCFR.hpp
+++ cpp_src/DeepMCCFR.hpp
@@ -6,6 +6,7 @@
 #include <memory>
 #include <random>
 #include <atomic>
+#include <functional> // <-- Добавим functional
 
 #include <pybind11/pybind11.h>
 #include "game_state.hpp"
@@ -17,20 +18,18 @@ namespace py = pybind11;
 
 namespace ofc {
 
-using LogQueue = py::object;
-// --- ИЗМЕНЕНИЕ: Добавляем тип для персональной очереди результатов ---
-using PersonalResultQueue = py::object;
+// --- ИЗМЕНЕНИЕ: Определяем типы для коллбэков ---
+using RequestCallback = std::function<void(uint64_t, bool, const std::vector<float>&, const std::vector<std::vector<float>>&)>;
+using ResultCallback = std::function<py::tuple(uint64_t)>;
 
 class DeepMCCFR {
 public:
+    // --- ИЗМЕНЕНИЕ: Конструктор теперь принимает коллбэки ---
     DeepMCCFR(size_t action_limit,
               SharedReplayBuffer* policy_buffer, 
               SharedReplayBuffer* value_buffer, 
-              InferenceRequestQueue* request_queue,
-              // --- ИЗМЕНЕНИЕ: Принимаем указатель на персональную очередь и ID ---
-              PersonalResultQueue* personal_result_queue,
-              uint64_t worker_id,
-              LogQueue* log_queue);
+              RequestCallback request_cb,
+              ResultCallback result_cb);
     
     void run_traversal();
 
@@ -39,11 +38,9 @@ private:
     SharedReplayBuffer* policy_buffer_;
     SharedReplayBuffer* value_buffer_;
     
-    InferenceRequestQueue* request_queue_;
-    // --- ИЗМЕНЕНИЕ: Храним указатель на свою очередь и свой ID ---
-    PersonalResultQueue* personal_result_queue_;
-    uint64_t worker_id_;
-    LogQueue* log_queue_;
+    // --- ИЗМЕНЕНИЕ: Храним коллбэки ---
+    RequestCallback request_cb_;
+    ResultCallback result_cb_;
 
     size_t action_limit_;
     std::mt19937 rng_;
